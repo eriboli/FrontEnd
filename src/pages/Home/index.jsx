df450b48e119346ca36dc496fc53114dfb62f929
Eduardo Riboli <eriboli@gmail.com>
2026-01-02 22:53:28 -0300

    Turn language interface pt-br again

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -61,8 +61,8 @@ function Home() {
       <div className='container'>
         <div className='header-container'>
           <div >
-            <h1>Users list</h1>
-            <p>Manage the registered users</p>
+            <h1>Lista de Usuários</h1>
+            <p>Gerencie os usuários cadastrados</p>
           </div>
           <button onClick={() => setOpenModalAddUser(!openModalAddUser)}><img src={Add} /></button>
         </div>
@@ -71,8 +71,8 @@ function Home() {
 
           <div key={user.codigo} className='user-card'>
             <div>
-              <p>Name: <span>{user.name}</span></p>
-              <p>Age: <span>{user.age}</span></p>
+              <p>Nome: <span>{user.name}</span></p>
+              <p>Idade: <span>{user.age}</span></p>
               <p>Email: <span>{user.email}</span></p>
             </div>
             <div>
